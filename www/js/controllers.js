5b7a19b2f97d3fca29fb1f477f2981c01a862e4d
Muigai <muigaiunaka@gmail.com>
2016-08-10 01:58:15 -0400

    work work work work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,17 +25,20 @@ angular.module('nifty.controllers', ['ngOpenFB'])
         function (response) {
           if (response.status === 'connected') {
             console.log('Facebook login succeeded');
+            $scope.loggedin = true;
+            $scope.getInfo();
+            $scope.swiper.slideNext();
+            $scope.swiper.allowSwipeToPrev = false;
             /*$state.go('tab-home');*/
           } else {
             alert('Facebook login failed');
+            $scope.loggedin = false;
           }
         }
       );
     };
     /** Function to retrieve the user's facebook information after they login */
     $scope.getInfo = function () {
-      /*Getter method to get information of me (user who is logged in)*/
-      // ngFB.api('/me', 'GET', {fields: 'first_name,last_name,name,picture.width(300).height(300)'},
       ngFB.api({
         path: '/me',
         params: {fields: 'first_name,last_name,id,name,picture.width(300).height(300)'}
@@ -44,11 +47,9 @@ angular.module('nifty.controllers', ['ngOpenFB'])
           $scope.user.name = response.first_name + " " + response.last_name.substring(0,1);
           $scope.user.fbpic = response.picture.data.url;
           console.log($scope.user);
-          $scope.swiper.slideNext();
-          $scope.swiper.allowSwipeToPrev = false;
-
         } )
     };
+    
     $scope.profileComplete = function() {
       if($scope.user.name !== undefined && $scope.user.bio !== undefined && $scope.user.address !== undefined) {
         $scope.swiper.slideNext();
@@ -56,39 +57,9 @@ angular.module('nifty.controllers', ['ngOpenFB'])
       } else {
         console.log("Still need to fill in profile info!")
       }
-    }
+    };
 
   })
-
-/*  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, ngFB, $state, userData) {
-    /!** Function to login the user into the app using facebook*!/
-    $scope.fbLogin = function () {
-      ngFB.login({scope: 'email'}).then(
-        function (response) {
-          if (response.status === 'connected') {
-            console.log('Facebook login succeeded');
-            $state.go('profile');
-          } else {
-            alert('Facebook login failed');
-          }
-        }
-      );
-    };
-    /!** Function to retrieve the user's facebook information after they login *!/
-    $scope.getInfo = function () {
-      /!*Getter method to get information of me (user who is logged in)*!/
-      // ngFB.api('/me', 'GET', {fields: 'first_name,last_name,name,picture.width(300).height(300)'},
-        ngFB.api({
-          path: '/me',
-          params: {fields: 'first_name,last_name,id,name,picture'}
-        }).then(
-        function(response) {
-          var formattedName = response.first_name + " " + response.last_name.substring(0,1);
-          localStorage.setItem('name', formattedName);
-          localStorage.setItem('fbpic', JSON.stringify(response.picture.data.url));
-        } )
-    }
-  })*/
 
   .controller('HomeCtrl', function($scope, Home) {
     $scope.home = Home.all();
@@ -99,23 +70,6 @@ angular.module('nifty.controllers', ['ngOpenFB'])
   .controller('SearchCtrl', function($scope) {})
 
   .controller('MessagesCtrl', function($scope) {})
-
-  // .controller('LoginCtrl', function($scope) {
-  //   $scope.name = 'login please';
-  //   $scope.FBLogin = function() {
-  //     FB.login(function(response) {
-  //       if (response.status === 'connected') {
-  //         console.log("connected to fb");
-  //         var accessToken = FB.getAuthResponse();
-  //         console.log(accessToken);
-  //       } else if (response.status === 'not_authorized') {
-  //         console.log("not auth'd to connect to fb");
-  //       } else {
-  //         console.log("not connected to fb");
-  //       }
-  //     })
-  //   }
-  // })
 
   .controller('ChatsCtrl', function($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
